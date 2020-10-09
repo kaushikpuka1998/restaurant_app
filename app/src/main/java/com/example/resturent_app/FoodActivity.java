ba09f1c51515953ee5898017afd9441fe143578b
Kaushik Ghosh <kaushikghosh199832@gmail.com>
2020-10-09 18:29:32 +0530

    Added Bottom_sheet_dialog and MAking Grand_total

--- app/src/main/java/com/example/resturent_app/FoodActivity.java
+++ app/src/main/java/com/example/resturent_app/FoodActivity.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class FoodActivity extends AppCompatActivity {
 
@@ -25,14 +28,14 @@ public class FoodActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
 
         itemname = findViewById(R.id.fooditemname);
         itemprice = findViewById(R.id.foodItemprice);
         itemrating = findViewById(R.id.foodRating);
         itempic =  findViewById(R.id.fooditempic);
         ratingBar = findViewById(R.id.foodratingBar);
-        addtocart = findViewById(R.id.FoodCartaddButton);
+        addtocart = findViewById(R.id.foodaddtocart);
         back = findViewById(R.id.foodBackbutton);
 
 
@@ -53,6 +56,69 @@ public class FoodActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+
+        addtocart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(FoodActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(FoodActivity.this);
+                bottomSheetDialog.setContentView(R.layout.bottomsheetdialog);
+                bottomSheetDialog.show();//If  .show not used then bottomsheet will not come
+                bottomSheetDialog.setCanceledOnTouchOutside(true);
+
+                TextView bottomitemname = bottomSheetDialog.findViewById(R.id.bottomitemname);
+                final TextView bottomprice = bottomSheetDialog.findViewById(R.id.bottomprice);
+                ImageButton bottomplus =  bottomSheetDialog.findViewById(R.id.bottomplus);
+                ImageButton bottomminus =  bottomSheetDialog.findViewById(R.id.bottomminus);
+                final TextView bottomcount =bottomSheetDialog.findViewById(R.id.bottomcount);
+                final TextView bottomsheet_grantotal = bottomSheetDialog.findViewById(R.id.bottomsheet_grantotal);
+
+                Button pay_here = bottomSheetDialog.findViewById(R.id.bottom_pay_here);
+
+                bottomitemname.setText(intent.getStringExtra("name"));
+                bottomprice.setText(intent.getStringExtra("price"));
+                bottomsheet_grantotal.setText(intent.getStringExtra("price"));
+
+                bottomplus.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(FoodActivity.this, "Added", Toast.LENGTH_SHORT).show();
+                        String count = bottomcount.getText().toString();
+
+                        String itemvalue =  intent.getStringExtra("price");
+                        int itempvalint = Integer.parseInt(itemvalue);
+                        int cnt = Integer.parseInt(count);
+                        if(cnt<6)
+                        {
+                            cnt++;
+                            int tot =(cnt*itempvalint);
+                            bottomcount.setText(String.valueOf(cnt));
+                            System.out.println(tot);
+                            bottomsheet_grantotal.setText(String.valueOf(tot));
+                        }else
+                        {
+                            Toast.makeText(FoodActivity.this, "You can Buy Highest 6 Quantity", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+                bottomminus.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(FoodActivity.this, "Discarded One", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+
+
+
+
+
+
             }
         });
 
